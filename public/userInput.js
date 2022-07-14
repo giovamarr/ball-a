f95ac777a67225274e008b2e82ecf0e73da4232d
Giovanni Martin <giovamartin@gmail.com>
2022-07-13 22:44:00 -0300

    input with letters

--- public/userInput.js
+++ public/userInput.js
@@ -3,31 +3,31 @@ let justPressed = false;
 //Event listeners for the arrow keys
 function userInput(obj){
     canvas.addEventListener('keydown', function(e){
-        if(e.keyCode === 37){
+        if(e.keyCode === 37  || e.keyCode === 65){
             if(obj.left === false){
                 justPressed = true;
             }
             obj.left = true;
         }
-        if(e.keyCode === 38){
+        if(e.keyCode === 38  || e.keyCode === 87){
             if(obj.up === false){
                 justPressed = true;
             }
             obj.up = true;
         }
-        if(e.keyCode === 39){
+        if(e.keyCode === 39 || e.keyCode === 68){
             if(obj.right === false){
                 justPressed = true;
             }
             obj.right = true;
         }
-        if(e.keyCode === 40){
+        if(e.keyCode === 40 || e.keyCode === 83){
             if(obj.down === false){
                 justPressed = true;
             }
             obj.down = true;
         }
-        if(e.keyCode === 32){
+        if(e.keyCode === 32|| e.keyCode === 88){
             if(obj.action === false){
                 justPressed = true;
             }
@@ -40,19 +40,19 @@ function userInput(obj){
     });
     
     canvas.addEventListener('keyup', function(e){
-        if(e.keyCode === 37){
+        if(e.keyCode === 37 || e.keyCode === 65){
             obj.left = false;
         }
-        if(e.keyCode === 38){
+        if(e.keyCode === 38 || e.keyCode === 87){
             obj.up = false;
         }
-        if(e.keyCode === 39){
+        if(e.keyCode === 39 || e.keyCode === 68){
             obj.right = false;
         }
-        if(e.keyCode === 40){
+        if(e.keyCode === 40 || e.keyCode === 83){
             obj.down = false;
         }
-        if(e.keyCode === 32){
+        if(e.keyCode === 32 || e.keyCode === 88){
             obj.action = false;
         }
         emitUserCommands(obj);
